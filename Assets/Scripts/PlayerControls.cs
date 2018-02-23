95bcd4e6e8e69dba0528e54fd737df1252835a71
redligot2009 <redligot2009.forreal@gmail.com>
2018-02-23 19:59:56 +0800

    stuff added

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    PhysicsObject physObj;
+    public float jumpSpeed = 8f, moveSpeed = 8f;
+    
+	void Start () {
+        physObj = GetComponent<PhysicsObject>();
+	}
+	
+	void Update () {
+
+		if(Input.GetKey(KeyCode.A) && !physObj.leftwall)
+        {
+            physObj.velocity.x = Mathf.Lerp(physObj.velocity.x,-moveSpeed,Time.deltaTime*5f);
+        }
+        else if(Input.GetKey(KeyCode.D) && !physObj.rightwall)
+        {
+            physObj.velocity.x = Mathf.Lerp(physObj.velocity.x, moveSpeed, Time.deltaTime * 5f);
+        }
+        else
+        {
+            physObj.velocity.x = Mathf.Lerp(physObj.velocity.x, 0f, Time.deltaTime * 10f);
+        }
+        if(Input.GetKey(KeyCode.Space) && physObj.grounded)
+        {
+            physObj.velocity.y = jumpSpeed;
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            if(physObj.velocity.y > 0)
+            {
+                physObj.velocity.y = physObj.velocity.y * 0.5f;
+            }
+        }
+	}
+}
