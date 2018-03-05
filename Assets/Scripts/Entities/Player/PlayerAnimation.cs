241ffc500b4f91effff7e2a6b28c2e660133b4c0
redligot2009 <redligot2009.forreal@gmail.com>
2018-03-06 03:02:25 +0800

    removed animations

--- Assets/Scripts/Entities/Player/PlayerAnimation.cs
+++ Assets/Scripts/Entities/Player/PlayerAnimation.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour {
     
     PhysicsObject po;
-    Animator anim;
-    Transform animobj;
 
 	void Start () {
         po = GetComponent<PhysicsObject>();
-        animobj = transform.Find("animations");
-        anim = animobj.GetComponent<Animator>();
 	}
 	
 	void Update ()
     {
-        anim.SetFloat("xspeed", Mathf.Abs(po.velocity.x));
-        anim.SetFloat("yspeed", Mathf.Abs(po.velocity.y));
-	}
+    }
 }
