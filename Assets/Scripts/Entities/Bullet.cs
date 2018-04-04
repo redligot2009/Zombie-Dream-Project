5197345c98278cbf0c60cccaf90733496ebd4488
redligot2009 <redligot2009.forreal@gmail.com>
2018-04-05 01:42:20 +0800

    shooting function works

---
+++ Assets/Scripts/Entities/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public LayerMask tileLayers;
+    public float lifeTime = 2f;
+    float lifeTimer = 0;
+
+    bool Contains(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("YO");
+        if(Contains(tileLayers, collision.gameObject.layer))
+        {
+            Destroy(gameObject);
+        }
+    }
+    public float moveSpeed = 5f;
+    void Start () {
+        lifeTimer = lifeTime;
+	}
+	
+	void Update () {
+        transform.Translate(new Vector3(moveSpeed * Time.deltaTime,0));
+        if (lifeTimer > 0)
+            lifeTimer -= Time.deltaTime;
+        if(lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
