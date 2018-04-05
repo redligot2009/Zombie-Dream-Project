298afadfd056beb5eb26cc89ac761877ec75f091
redligot2009 <redligot2009.forreal@gmail.com>
2018-04-06 01:42:52 +0800

    There are zombies now
    also placed animation logic of player in PlayerAnimation

--- Assets/Scripts/Entities/Bullet.cs
+++ Assets/Scripts/Entities/Bullet.cs
@@ -15,7 +15,6 @@ public class Bullet : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("YO");
         if(Contains(tileLayers, collision.gameObject.layer))
         {
             Destroy(gameObject);
