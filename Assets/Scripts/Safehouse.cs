50e77fd2cc131f83941ad9dbd779f6f3421fb424
redligot2009 <redligot2009.forreal@gmail.com>
2018-04-11 01:28:48 +0800

    added safehouse feature

--- Assets/Scripts/Safehouse.cs
+++ Assets/Scripts/Safehouse.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(Rigidbody2D))]
 public class Safehouse : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public string nextSceneName;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("player"))
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+    }
 	
 	// Update is called once per frame
 	void Update () {
