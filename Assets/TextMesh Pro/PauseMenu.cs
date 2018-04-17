d9b5686ded5d6ce0ee7807ab750a1b9e47d8661c
Nate122798 <36832215+Nate122798@users.noreply.github.com>
2018-04-17 09:12:20 +0800

    Pause menu
    ESC NIGGA PAUSES

---
+++ Assets/TextMesh Pro/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+
+    public static bool PausedNaAngLaro = false;
+
+    public GameObject pauseMenuUI;
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedNaAngLaro)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+		
+	}
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PausedNaAngLaro = false;
+    }
+     void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PausedNaAngLaro = true;
+    }
+    public void Load()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
