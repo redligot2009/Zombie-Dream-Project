937a14f825ca81454f8dcab64514ad50ed0c1703
Nate122798 <36832215+Nate122798@users.noreply.github.com>
2018-04-17 11:36:34 +0800

    pAuSED

--- Assets/TextMesh Pro/PauseMenu.cs
+++ Assets/TextMesh Pro/PauseMenu.cs
@@ -2,46 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour {
 
-public class PauseMenu : MonoBehaviour
-{
-
-
-    public static bool PausedNaAngLaro = false;
+    public static bool GamePaused = false;
 
     public GameObject pauseMenuUI;
+
 	// Update is called once per frame
 	void Update ()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (PausedNaAngLaro)
+            if (GamePaused)
             {
                 Resume();
             }
             else
             {
-                Paused();
+                Pause();
             }
-        }
-		
+        }	
 	}
-    public void Resume()
+     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        PausedNaAngLaro = false;
+        GamePaused = false;
     }
-     void Paused()
+     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        PausedNaAngLaro = true;
+        GamePaused = true;
     }
     public void Load()
     {
-        Time.timeScale = 1f;
+        
         SceneManager.LoadScene("Menu");
     }
-    
 }
