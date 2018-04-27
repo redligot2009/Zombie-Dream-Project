0c5642b476a38a1ac2c665ead474ba6667899166
redligot2009 <redligot2009.forreal@gmail.com>
2018-04-27 18:32:30 +0800

    rearranged scripts

---
+++ Assets/Scripts/Entities/ScriptableObjects/Editor/ItemEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ItemObject))]
+public class ItemEditor : Editor
+{
+    public SerializedProperty
+        itemType,
+        pickUpType,
+        healthRestore,
+        associatedWeapon,
+        objectSprite;
+    private void OnEnable()
+    {
+        pickUpType = serializedObject.FindProperty("pickUpType");
+        itemType = serializedObject.FindProperty("itemType");
+        healthRestore = serializedObject.FindProperty("healthRestore");
+        associatedWeapon = serializedObject.FindProperty("associatedWeapon");
+        objectSprite = serializedObject.FindProperty("objectSprite");
+    }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        ItemObject.ItemType itype = (ItemObject.ItemType)itemType.enumValueIndex;
+        EditorGUILayout.ObjectField(objectSprite, typeof(Sprite));
+        EditorGUILayout.PropertyField(pickUpType);
+        EditorGUILayout.PropertyField(itemType);
+        switch (itype)
+        {
+            case ItemObject.ItemType.HEALTH_PICKUP:
+                int restoreVal = EditorGUILayout.IntField(new GUIContent("Health Restore", "Amount of health to restore"), healthRestore.intValue);
+                healthRestore.intValue = restoreVal;
+                break;
+            case ItemObject.ItemType.ITEM_PICKUP:
+                EditorGUILayout.ObjectField(associatedWeapon);
+                break;
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
